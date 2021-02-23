3f5108b4c046410bba5da7e22d646d0ca720b158
AlienDev66 <p.capitango65@gmail.com>
2021-02-23 23:18:56 +0100

    :sparkles: Creating some components like the Profile an How much challenge complete.

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/pages/Home.module.css";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import CompletedChallenges from "../components/CompletedChallenges";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
