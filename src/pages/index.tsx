fa7e101fe6b531c5073b13bcfe570e6ece1cbe9f
AlienDev66 <p.capitango65@gmail.com>
2021-02-24 00:00:13 +0100

    :sparkles: Creating countdown component and basic functionality.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,23 @@ import styles from "../styles/pages/Home.module.css";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Exercise.me </title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
